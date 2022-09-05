4c0480224514137e50f74327fb69be324ab0f602
BhatiaUday <101352755+BhatiaUday@users.noreply.github.com>
2022-09-05 13:37:15 +0530

    added new emp and new cust functions

--- frontend.py
+++ frontend.py
@@ -1,7 +1,9 @@
 #Music store FRONT END
 
+from turtle import ycor
 import mysql.connector as mysql
 import getpass
+import os
 uname=input("Username(root is default)") or "root"
 pwd=getpass.getpass("Pwd(No default value)") or "wysiwygmn@36"
 serverip=input("Server address(enter for default)") or "localhost"
@@ -9,11 +11,11 @@ cnx = mysql.connect(user=uname, password=pwd,host=serverip)
 cursor= cnx.cursor()
 cursor.execute("use testpylink")
 try:
-    cursor.execute("create table custinfo(custid int PRIMARY KEY,name varchar(40),phone_no int,address varchar(50))")
+    cursor.execute("create table custinfo(custid int PRIMARY KEY,name varchar(40),phone_no bigint(10),address varchar(100))")
 except:
     pass
 try:
-    cursor.execute("create table empinfo(empid int PRIMARY KEY ,empname varchar(40),phone_no int,address varchar(50))")
+    cursor.execute("create table empinfo(empid int PRIMARY KEY ,empname varchar(40),phone_no bigint(10),address varchar(100))")
 except:
     pass
 try:
@@ -31,22 +33,45 @@ def cprint(cursorfx):
 def menu():
     print("1.Make a sale")
     print("2.Add new Custumer")
-    print("3.Cancel/refund a sale")
+    print("3.Cancel a sale")
     print("4.Update Custumer info")    
     print("5.Add Employee")
     print("6.Update Employee info")
-    print("7.Quit")
+    print("7.Display all Sales")
+    print("8.Display all Custumers")
+    print("9.Display all employees")
+    print("0.Quit")
 
-currentsale = 0
-cursor.execute("select MAX(order_id) from Sales")
+currentsale = 1
+cursor.execute("select MAX(order_id) from sales")
 for x in cursor:
-    stripped=str(x).replace('(','').replace(')','').replace(',','')
+    strippedx=str(x).replace('(','').replace(')','').replace(',','')
+    print(strippedx,"1st")
     try:
-        currentsale=currentsale+int(stripped)+1
+        currentsale=currentsale+int(strippedx)+1
     except:
         pass
-print(currentsale)
-    
+print(currentsale,"2nd")
+currentcuid = 1
+cursor.execute("select MAX(custid) from custinfo")
+for y in cursor:
+    strippedy=str(y).replace('(','').replace(')','').replace(',','')
+    print(strippedy,"3rd")
+    try:
+        currentcuid=currentcuid+int(strippedy)
+    except:
+        pass
+print(currentcuid,"4th")
+currentempid =1
+cursor.execute("select MAX(empid) from empinfo")
+for z in cursor:
+    strippedz=str(z).replace('(','').replace(')','').replace(',','')
+    print(strippedz,"5th")
+    try:
+        currentempid=currentempid+int(strippedz)
+    except:
+        pass
+print(currentempid,"6th")
 
 def sale():
     global currentsale
@@ -61,21 +86,50 @@ def sale():
     cnx.commit()
     currentsale = currentsale + 1
      
-def cust():
-    custid=int(input("enter custid"))
-    cname=input("enter customer name")
-    cphone_no=int(input("enter customer phone no"))
-    caddress=input("enter customer address")
+def newcust():
+    global currentcuid
+    cname=input("Enter Custumer Name: ")
+    c_no=int(input("Enter Customer Phone Number: "))
+    caddr=input("Enter Customer Address: ")
+    data=str(currentcuid)+","+"'"+str(cname)+"'"+","+str(c_no)+","+"'"+str(caddr)+"'"
+    query="insert into custinfo values ("+data+")"
+    cursor.execute(query)
+    cnx.commit()
+    currentcuid = currentcuid + 1
+def newemp():
+    global currentempid
+    ename=input("Enter Employee Name: ")
+    e_no=int(input("Enter Employee Phone Number: "))
+    eaddr=input("Enter Employee Address: ")
+    data=str(currentempid)+","+"'"+str(ename)+"'"+","+str(e_no)+","+"'"+str(eaddr)+"'"
+    query="insert into empinfo values ("+data+")"
+    cursor.execute(query)
+    cnx.commit()
+    currentempid = currentempid + 1
 
-def emp():
-    empid=int(input("enter empid"))
-    empname=input("enter empname")
-    phone_no=int(input("enter phone (keep it in 10 digits"))
-    address=input("enter address as city_area")
-cursor.execute("show tables")
-cprint(cursor)
-
-menu()
-choice=int(input("Enter Choice: "))
-if choice==1:
-    sale()
+while True:
+    menu()
+    choice=int(input("Enter Choice: "))
+    if choice==1:
+        sale()
+    if choice==2:
+        newcust()
+    if choice==3:
+        pass #remove sale
+    if choice==4:
+        pass # update cinfo
+    if choice==5:
+        newemp()
+    if choice==6:
+        pass # update einfo
+    if choice==7:
+        cursor.execute("select * from sales")
+        cprint(cursor)
+    if choice==8:
+        cursor.execute("select * from custinfo")
+        cprint(cursor)
+    if choice==9:
+        cursor.execute("select * from empinfo")
+        cprint(cursor)
+    if choice==0:
+        os._exit(1)
